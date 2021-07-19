ed6e51f2efd4803041177c153c069f03168789c7
Borman Karlovich <mrgabrielripper@gmail.com>
2021-07-19 20:43:45 +0600

    Cards generated via db

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,4 +1,4 @@
-// import checkNumInputs from './checkNumInputs';
+import {postData} from '../services/requests';
 
 const forms = () => {
     const form = document.querySelectorAll('form'),
@@ -21,15 +21,6 @@ const forms = () => {
         question: 'assets/question.php'
     };
 
-    const postData = async (url, data) => {
-        let res = await fetch(url, {
-            method: 'POST',
-            body: data
-        });
-
-        return await res.text();
-    };
- 
     const clearInputs = () => {
         inputs.forEach(item => {
             item.value = '';
--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,18 +1,47 @@
-const showMoreStyles = (trigger, styles) => {
-    const cards = document.querySelectorAll(styles),
-          btn = document.querySelector(trigger);
+import { getResourse } from "../services/requests";
 
-    cards.forEach(card => {
-        card.classList.add('animated', 'fadeInUp');
+const showMoreStyles = (trigger, wrapper) => {
+    const btn = document.querySelector(trigger);
+
+    // через работу с версткой и стилями
+    // cards.forEach(card => {
+    //     card.classList.add('animated', 'fadeInUp');
+    // });
+
+    // btn.addEventListener('click', () => {
+    //     cards.forEach(card => {
+    //         card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
+    //         card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+    //     });
+    //     btn.remove();
+    // });
+
+    //через json-server
+    btn.addEventListener('click', function() {
+        getResourse('http://localhost:3000/styles')
+            .then(res => createCards(res))
+            .catch(error => console.log(error));
+
+        this.remove();
     });
 
-    btn.addEventListener('click', () => {
-        cards.forEach(card => {
-            card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
-            card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+    function createCards(response) {
+        response.forEach(({src, title, link}) => {
+            let card = document.createElement('div');
+
+            card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+
+            card.innerHTML = `
+                <div class="styles-block">
+                    <img src=${src} alt="style">
+                    <h4>${title}</h4>
+                    <a href=${link}>Подробнее</a>
+                </div>
+            `;
+
+            document.querySelector(wrapper).appendChild(card);
         });
-        btn.remove();
-    });
+    }
 };
 
 export default showMoreStyles;
