dd4678df9f5511fbae29b0cfcc8bc14376d50684
Borman Karlovich <mrgabrielripper@gmail.com>
2021-07-19 20:05:25 +0600

    When click on the button, more cards appear

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
 import forms from './modules/forms';
+import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
+import showMoreStyles from './modules/showMoreStyles';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -9,4 +12,8 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
     forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
+    showMoreStyles('.button-styles', '.styles-2');
 });
