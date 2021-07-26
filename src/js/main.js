73c9cf9efa636bceac165a05a9b699df82d3feaf
Borman Karlovich <mrgabrielripper@gmail.com>
2021-07-26 20:32:15 +0600

    Dragndrop function implemented

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,7 @@ import pictureSize from './modules/pictureSize';
 import accordion from './modules/accordion';
 import burger from './modules/burger';
 import scrolling from './modules/scrolling';
+import drop from './modules/drop';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -30,4 +31,5 @@ window.addEventListener('DOMContentLoaded', () => {
     accordion('.accordion-heading', '.accordion-block');
     burger('.burger-menu', '.burger');
     scrolling('.pageup');
+    drop();
 });
